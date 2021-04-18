bb92287074ab26b298bbe6e974ff6dc1c1f62936
Prashant Gaur <prashant.gaur@de.bosch.com>
2021-04-18 19:01:42 +0200

    Add unit-tests for positive test case

---
+++ tests/Main.cpp
@@ -0,0 +1,8 @@
+#include <gmock.h>
+#include <gtest.h>
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/Unittests.cpp
@@ -0,0 +1,27 @@
+#include "DiscoverIntention.h"
+#include <gtest.h>
+#include <string>
+#include <tuple>
+
+class DiscoverIntentionParametersTests : public ::testing::TestWithParam<std::tuple<std::string, std::string> > {
+protected:
+    DiscoverIntention tool;
+};
+
+INSTANTIATE_TEST_CASE_P(
+    DiscoverIntentionTests,
+    DiscoverIntentionParametersTests,
+    ::testing::Values(
+        std::make_tuple("Get Weather", "How is the weather in my city ?"),
+        std::make_tuple("Check Calendar", "Do I have appointment at 06:00pm today ?")));
+
+TEST_P(DiscoverIntentionParametersTests, MatchIntention)
+{
+    //setup
+    std::string expected = std::get<0>(GetParam());
+    std::string input = std::get<1>(GetParam());
+    //action
+    std::string actual = tool.find_intention(input);
+    //assert
+    ASSERT_EQ(expected, actual);
+}
